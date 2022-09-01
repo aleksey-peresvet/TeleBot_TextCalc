70c3ebbbb0f37638e3c2aad79203f55b8a535ddd
aleksey-peresvet <aleksey.peresvet@gmaol.com>
2022-09-01 07:07:42 +0400

    Телеграм Бот - текстовый калькулятор, версия 1.0

---
+++ Calculator_OPZ.cs
@@ -0,0 +1,158 @@
+﻿using System.Text.RegularExpressions;
+
+namespace TelegramBot_Arithmetic_Calculator
+{
+    internal class Calculator_OPZ
+    {
+        private Stack<string> result;
+
+        private bool IsArithmetic(string input)
+        {
+            Regex regex = new Regex("[^0-9\\+\\-\\*\\/\\^\\%\\)\\(\\,\\.]");
+            return !regex.IsMatch(input);
+        }
+        private bool IsNumber(string input)
+        {
+            Regex regex = new Regex("([\\d\\.\\d]+)");
+            return regex.IsMatch(input);
+        }
+        private MatchCollection get_elements(string input)
+        {
+            Regex args = new Regex("([\\+|\\-|\\*|\\/|\\^|\\%|\\(|\\)]|[\\d\\,\\.\\d]+)");
+            return args.Matches(input);
+        }
+
+        public string Calculate_poliz(ref Stack<string> stack)
+        {
+            if (stack.Count == 0)
+            {
+                return "Error01";
+            }
+            List<string> poliz = stack.ToList();
+            poliz.Reverse();
+            Stack<string> temp_stack = new Stack<string>();
+            foreach(var element in poliz)
+            {
+                if (IsNumber(element))
+                {
+                    temp_stack.Push(element);
+                }
+                if (!IsNumber(element))
+                {
+                    double[] arg = { 0, 0 };
+                    try
+                    {
+                        arg[1] = Convert.ToDouble(temp_stack.Pop());
+                        arg[0] = Convert.ToDouble(temp_stack.Pop());
+                    }
+                    catch
+                    {
+                        return "Обнаружена некорректная запись числа!";
+                    }
+                    switch(element)
+                    {
+                        case "+": temp_stack.Push((arg[0] + arg[1]).ToString());
+                            break;
+                        case "-": temp_stack.Push((arg[0] - arg[1]).ToString());
+                            break;
+                        case "*": temp_stack.Push((arg[0] * arg[1]).ToString());
+                            break;
+                        case "/": temp_stack.Push((arg[0] / arg[1]).ToString());
+                            break;
+                        case "^": temp_stack.Push((Math.Pow(arg[0], arg[1])).ToString());
+                            break;
+                        case "%": temp_stack.Push((arg[0] * arg[1] / 100).ToString());
+                            break;
+                    }
+                }
+            }
+            return temp_stack.Pop();
+        }
+        public ref Stack<string> Parser_poliz(string input_string)
+        {
+            result = new Stack<string>();
+            if (!IsArithmetic(input_string)) return ref result;
+            input_string = input_string.Trim();//Remove whitespaces
+            if(input_string.Contains("**")) input_string.Replace("**", "^");//Replace two char operator with one char
+            MatchCollection elements = get_elements(input_string);//Parse input string with regex
+            Stack<string> stack = new Stack<string>();
+            foreach (Match match in elements)
+            {
+                if (IsNumber(match.Value))
+                {
+                    result.Push(match.Value);
+                }
+                else
+                {
+                    //Low priority
+                    if (match.Value == "+" || match.Value == "-")
+                    {
+                        if(stack.Count>0)
+                        {
+                            while (stack.Peek() == "+" || stack.Peek() == "-" ||
+                            stack.Peek() == "*" || stack.Peek() == "/" ||
+                            stack.Peek() == "^" || stack.Peek() == "%")
+                            {
+                                result.Push(stack.Pop());
+                                if (stack.Count == 0) break;
+                            }
+                        }
+                        stack.Push(match.Value);
+                        continue;
+                    }
+                    //Medium priority
+                    if (match.Value == "*" || match.Value == "/")
+                    {
+                        if (stack.Count > 0)
+                        {
+                            while (stack.Peek() == "*" || stack.Peek() == "/" ||
+                            stack.Peek() == "^" || stack.Peek() == "%")
+                            {
+                                result.Push(stack.Pop());
+                                if (stack.Count == 0) break;
+                            }
+                        }
+                        stack.Push(match.Value);
+                        continue;
+                    }
+                    //High priotity
+                    if (match.Value == "^" || match.Value == "(" ||
+                        match.Value == "%")
+                    {
+                        if (stack.Count > 0 && 
+                            (match.Value == "^" || match.Value == "%"))
+                        {
+                            while (stack.Peek() == "^" || stack.Peek() == "%")
+                            {
+                                result.Push(stack.Pop());
+                                if (stack.Count == 0) break;
+                            }
+                        }
+                        stack.Push(match.Value);
+                        continue;
+                    }
+                    // Close (
+                    if (match.Value == ")")
+                    {
+                        if (stack.Count > 0)
+                        {
+                            while (stack.Peek() != "(")
+                            {
+                                result.Push(stack.Pop());
+                                if (stack.Count == 0) break;
+
+                            }
+                            if (stack.Count > 0) stack.Pop();
+                            continue;
+                        }
+                    }
+                }
+            }
+            while (stack.Count > 0)
+            {
+                result.Push(stack.Pop());
+            }
+            return ref result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using Newtonsoft.Json;
+
+namespace TelegramBot_Arithmetic_Calculator
+{
+    class Program
+    {
+        static ITelegramBotClient bot = new TelegramBotClient("Здесь должен быть токен моего бота :)");
+        public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
+            {
+                var message = update.Message;
+                Calculator_OPZ opz = new Calculator_OPZ();
+                string answer = 
+                   opz.Calculate_poliz(ref opz.Parser_poliz(message.Text));
+                if (answer == "Error01")
+                {
+                    answer = "Мой функционал ограничен решением арифметических выражений.";
+                }
+                await botClient.SendTextMessageAsync(message.Chat, answer);
+            }
+        }
+
+        public static async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            Console.WriteLine(JsonConvert.SerializeObject(exception));
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Запущен бот " + bot.GetMeAsync().Result.FirstName);
+            var cts = new CancellationTokenSource();
+            var cancellationToken = cts.Token;
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = {  }
+            };
+            bot.StartReceiving(
+                HandleUpdateAsync,
+                HandleErrorAsync,
+                receiverOptions,
+                cancellationToken
+            );
+            Console.ReadLine();
+        }
+    }
+}
